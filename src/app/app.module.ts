2e70c41c6fe8db1a6055b7cb152103d9a24b2132
mrwogu <mail@wogu.pl>
2018-11-06 02:04:08 +0100

    Add Preboot

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TransferHttpCacheModule } from '@nguniversal/common';
+import { PrebootModule } from 'preboot';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,6 +16,7 @@ import { MainComponent } from './main/main.component';
   ],
   imports: [
     BrowserModule.withServerTransition({appId: 'angular7'}),
+    PrebootModule.withConfig({ appRoot: 'app-root' }),
     TransferHttpCacheModule,
     HttpClientModule,
     AppRoutingModule,
